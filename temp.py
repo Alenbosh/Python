4c0837092ed247260128bce06a58a5d04678c3fb
Alenbosh <reshmimanish1977@gmail.com>
2026-02-12 16:56:45 +0530

    Corrected few lines in temp file

--- temp.py
+++ temp.py
@@ -4,17 +4,18 @@ class Solution:
                 for i in range(len(nums)-1):
                     if (nums[i]>nums[i+1]):
                         nums[i],nums[i+1] =nums[i+1],nums[i]
-                print(nums[-1])
+                # print(nums[-1])
             else:
                 for i in range(len(nums)-1):
                     if (nums[i]>nums[i+1]):
                         nums[i],nums[i+1] =nums[i+1],nums[i]
-                print(nums[-1])
-                
+                # print(nums[-1])
+            return nums
     n=int(input("enter the size of an array:"))
     a=[]
     for i in range(n):
         k=int(input("enter the elements:"))
         a.append(k)
-    largestElement(1,a)
+    k = largestElement(1,a)
+    print(k)
     
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-                    if (nums[i]>nums[i+1]):
-                        nums[i],nums[i+1] =nums[i+1],nums[i]
-                print(nums[-1])
